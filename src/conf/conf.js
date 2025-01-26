f7555df21199c0e0597513a87827946599409097
Adeel <at4563323@gmail.com>
2025-01-26 20:45:37 +0500

    Update conf.js

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,7 +1,7 @@
 const conf = {
     
     apiUrl: (() => {
-        const baseUrl = String(import.meta.env.VITE_API_URL) || "https://fd02-154-208-41-213.ngrok-free.app";
+        const baseUrl = String(import.meta.env.VITE_API_URL) || "https://watchserver-irds.onrender.com";
         return baseUrl.endsWith('/api/v1') ? baseUrl : `${baseUrl}/api/v1`;
     })(),
     googleClientId: String(import.meta.env.VITE_GOOGLE_CLIENT_ID),
