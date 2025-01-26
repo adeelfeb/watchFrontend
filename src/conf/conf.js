82c07d2ea500f718c2cbe3b3e813086f3121f71b
adeelfeb <at4563323@gmail.com>
2025-01-26 14:11:01 +0500

    edit conf

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,7 +1,7 @@
 const conf = {
     
     apiUrl: (() => {
-        const baseUrl = String(import.meta.env.VITE_API_URL) ;
+        const baseUrl = String(import.meta.env.VITE_API_URL) || "https://fd02-154-208-41-213.ngrok-free.app";
         return baseUrl.endsWith('/api/v1') ? baseUrl : `${baseUrl}/api/v1`;
     })(),
     googleClientId: String(import.meta.env.VITE_GOOGLE_CLIENT_ID),
